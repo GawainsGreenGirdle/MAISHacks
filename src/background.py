be9a8b4a2fb6dbccb2c97adbbf25890ab0ed78ae
GawainsGreenGirdle <n1ghtmar3map@gmail.com>
2025-11-01 19:19:31 -0400

    Create script to generate map background
    To paste images using Pillow, we need a larger image on which we are
    pasting. Furthermore, we need a grid to ensure that our tiles are
    properly aligned.
    
    This grid can further be superimposed over the structure to provide
    clear squares for game purposes.
    
    This commit creates the script which generates a background from
    Blank.png tiles.

---
+++ src/background.py
@@ -0,0 +1,24 @@
+from PIL import Image as PILImage
+from urllib.request import urlopen
+from IPython.display import Image, display
+
+def background() -> PILImage.Image:
+  #sets width and height of new image to be 80*32 pixels
+  w = 80 * 32
+  h = 80 * 32
+  im = PILImage.new("RGBA", (w,h))
+
+  #uses url from github page to render blankTile.
+  url = "https://raw.githubusercontent.com/GawainsGreenGirdle/MAISHacks/main/Tokens/Blank.png"
+  blankTile = PILImage.open(urlopen(url))
+
+  #iterates from 0 to 32 (exclusive) to generate a 30X30 map with a border of 1
+  for i in range(32):
+    for j in range(32):
+      im.paste(blankTile, (i*80,j*80))
+
+  return im
+
+print(background())
+img = background()
+display(img)
