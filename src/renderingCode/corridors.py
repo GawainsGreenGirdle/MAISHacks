02a391424e57fe0c3738993cfe17e71a570d11f7
GawainsGreenGirdle <n1ghtmar3map@gmail.com>
2025-11-02 10:55:54 -0500

    Populate floors on map rendering
    This commit solves the floor issue and the corridor intersection issue
    by placing floor tiles along the length of the corridor.

--- src/renderingCode/corridors.py
+++ src/renderingCode/corridors.py
@@ -7,16 +7,20 @@ from addFunction import add
 
 def corridors(im1: PILImage.Image, matrix: np.array, listOfCorridors):
   url = "https://raw.githubusercontent.com/GawainsGreenGirdle/MAISHacks/main/Tokens/Wall.png"
+  url2= "https://raw.githubusercontent.com/GawainsGreenGirdle/MAISHacks/main/Tokens/FloorTile.png"
   wallTile = PILImage.open(urlopen(url))
   wallTileT = wallTile.transpose(PILImage.Transpose.ROTATE_90)
+  floorTile = PILImage.open(urlopen(url2))
   for corridor in listOfCorridors:
     length = corridor[2]-corridor[0]
     #dx = 1 if corridor[2] >= corridor[0] else -1
     height = corridor[3]-corridor[1]
     #dy = 1 if corridor[3] >= corridor[1] else -1
     for j in range(length):
+      addInexclusive(im1, floorTile, corridor[0]+j, float(corridor[1]))
       add(im1, wallTile, matrix, corridor[0]+j,float(corridor[1]+1)-12/80)
       add(im1, wallTile, matrix, corridor[0]+j,float(corridor[1])-12/80)
     for j in range(height):
-      add(im1, wallTileT, matrix, float(corridor[0]+1),corridor[1]+j)
-      add(im1, wallTileT, matrix, float(corridor[2])-24/80,corridor[1]+j)
+      addInexclusive(im1, floorTile, corridor[0], float(corridor[1])+j)
+      add(im1, wallTileT, matrix, float(corridor[0]+1)-12/80,corridor[1]+j)
+      add(im1, wallTileT, matrix, float(corridor[2])-12/80,corridor[1]+j)
